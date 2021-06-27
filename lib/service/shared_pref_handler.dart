0bda4bed4032996bb0d6222f5b27a96c7f0f8f14
ashu.aswal.333@gmail.com <ashu.aswal.333@gmail.com>
2021-06-27 20:14:45 +0530

    add shared preferences handler

---
+++ lib/service/shared_pref_handler.dart
@@ -0,0 +1,33 @@
+import 'package:demo/constants/shared_preferences_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefHandler{
+  static SharedPreferences? _sharedPrefInstance;
+
+  static Future<void> _init() async{
+    _sharedPrefInstance = await SharedPreferences.getInstance();
+  }
+
+  static Future<void> save({String userId = '', String userName = '', String acecessToken = ''})async{
+    if(_sharedPrefInstance == null)
+      await _init();
+    _sharedPrefInstance?.setString(USER_ID, userId);
+    _sharedPrefInstance?.setString(USER_NAME, userName);
+    _sharedPrefInstance?.setString(ACCESS_TOKEN, acecessToken);
+  }
+
+  static Future<void> delete()async{
+     if(_sharedPrefInstance == null)
+      await _init();
+    _sharedPrefInstance?.remove(USER_ID);
+    _sharedPrefInstance?.remove(USER_NAME);
+    _sharedPrefInstance?.remove(ACCESS_TOKEN);
+  }
+
+  static Future<bool> get exist async {
+    if(_sharedPrefInstance == null)
+      await _init();
+    final userId = _sharedPrefInstance?.getString(USER_ID) ?? null;
+    return userId != null;
+  }
+}
